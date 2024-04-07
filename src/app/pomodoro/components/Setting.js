8f743287e9cdf86804818dd1ea7b9cf04e416de5
TPHL1611 <tranphanhailong89@gmail.com>
2024-04-07 11:08:28 +0700

    #10 - replace bootstrap modal in pomodoro app with custom modal

---
+++ src/app/pomodoro/components/Setting.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Setting = React.forwardRef((props, ref) => {
+    return (
+        <div className="flex flex-col items-center">
+            <label htmlFor={props.inputID} className="text-sm font-mt-semibold text-black">
+                Thời gian
+            </label>
+            <input
+                id={props.inputID}
+                type="number"
+                className="w-full text-center border border-slate-950 rounded-[5px] mt-1 py-[5px] px-3"
+                ref={ref}
+                min="0"
+                value={props.valueTime}
+                onChange={(e) => props.setValueTime(e.target.value)}
+            />
+        </div>
+    );
+});
+export default Setting;
